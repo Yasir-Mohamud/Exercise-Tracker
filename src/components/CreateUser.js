be24aa7ee5a50eef26fd1830c499bb14f037c64a
Yasir-Mohamud <ymohamed018@gmail.com>
2022-01-03 14:39:44 -0800

    daily commit

--- src/components/CreateUser.js
+++ src/components/CreateUser.js
@@ -1,9 +1,47 @@
-import React from "react";
+import React, { useState } from "react";
 
 export default function CreateUser() {
+  const [user, setUser] = useState({ user: "" });
+
+  function onChangeUsername(e) {
+    setUser({
+      username: e.target.value,
+    });
+  }
+  function handleSubmit(e) {
+    e.preventDefault();
+    const newUser = {
+      username: user.username,
+    };
+    console.log(newUser);
+
+    setUser({
+      username: "",
+    });
+  }
+
   return (
     <div>
-      <p>You are on the Edit Exercise component!</p>
+      <h3>Create New User</h3>
+      <form onSubmit={handleSubmit}>
+        <div className="form-group">
+          <label>Username: </label>
+          <input
+            type="text"
+            required
+            className="form-control"
+            value={user.username}
+            onChange={onChangeUsername}
+          />
+        </div>
+        <div className="form-group">
+          <input
+            type="submit"
+            value="Create User"
+            className="btn btn-primary"
+          />
+        </div>
+      </form>
     </div>
   );
 }
